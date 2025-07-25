9e907e48e82ef8a039c676675edc94b147f954f2
vivek <tiwarivivek23@gmail.com>
2025-07-25 13:44:03 +0300

    app updated

---
+++ src/main/java/com/vivekt/viv/Viv.java
@@ -0,0 +1,9 @@
+package com.vivekt.viv;
+
+public class Viv {
+    public static void main(String[] args) {
+
+        System.out.println("Viv util coming..");
+
+    }
+}
---
+++ src/test/java/VivTest.java
@@ -0,0 +1,14 @@
+import com.vivekt.viv.Viv;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class VivTest {
+
+    @Test
+    void testVivMainApp(){
+
+        Viv.main(null);
+
+        Assertions.assertThatNoException();
+    }
+}
